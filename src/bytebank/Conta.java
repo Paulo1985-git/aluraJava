48fc52522dc067b1f774c849bf73abda48436b9e
Paulo1985-git <paulomattos.vv@gmail.com>
2023-03-17 17:48:14 -0300

    [1.0.0] - Fix: update class

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -37,5 +37,13 @@ public class Conta {
 		this.agencia = agencia;
 	}
 	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
 	
 }
