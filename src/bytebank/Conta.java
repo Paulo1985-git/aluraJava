c9075f409b76d460d279d60aaf98c6f5dcc218fa
Paulo1985-git <paulomattos.vv@gmail.com>
2023-03-18 23:12:50 -0300

    [1.0.0] - Fix: Polimorfismo

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -6,44 +6,45 @@ public class Conta {
 	private int agencia;
 	private int numero;
 	private Cliente titular;
-	
+
 	public void deposita(double valor) {
 		this.saldo += valor;
 	}
-	
+
 	public boolean saca(double valor) {
-		if(this.saldo >= valor) {
+		if (this.saldo >= valor) {
 			this.saldo -= valor;
 			return true;
 		}
 		return false;
 	}
+
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
+
 	public int getNumero() {
 		return numero;
 	}
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-	
+
 	public int getAgencia() {
 		return agencia;
 	}
-	
+
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
-	
+
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
-	
+
 	public Cliente getTitular() {
 		return titular;
 	}
-	
-	
+
 }
